51a50665bf3572e6d715dd733b34c1fb701fd6cf
root <root@s332.dhaval.com>
2018-05-17 03:07:54 -0400

    first commit

---
+++ hash.js
@@ -0,0 +1,79 @@
+﻿ 
+var bitcoin = require('bitcoin');
+var Client1 = require('node-rest-client').Client;
+var ServiceCall = new Client1();
+var fs = require('fs');
+var client = new bitcoin.Client({
+    host: '127.0.0.1',
+    port: 27504,
+    user: 'adminpc',
+    pass: 'admin'
+});
+var LastBlock=0;
+var CurrnectBlock = 100;
+var URL = "http://127.0.0.1/block/";
+
+
+function LoadFun(){
+    try{
+    
+    fs.readFile('./data.json', 'utf-8', function (err, data) {
+
+        if (data!="")
+        {
+            var arrayOfObjects = JSON.parse(data)
+            LastBlock = arrayOfObjects.LastBlock;
+
+            client.getInfo(function (err, data) {
+                CurrnectBlock = data.blocks;
+                console.log('LastBlock : ' + LastBlock + " CurrenctBlock : " + CurrnectBlock);
+                
+                if  (LastBlock!=CurrnectBlock){
+                    test(LastBlock);
+                }
+                else{
+                    setTimeout(function() {
+                       LoadFun() 
+                    }, 10000);
+                }
+            });
+    }
+        
+    })
+    } catch (e) {
+        console.log('Error : ' + LoadFun);
+    }
+
+   
+}
+function test(count) {
+    try{
+
+    if(count!=CurrnectBlock+1){
+  
+        client.GetBlockByNumber(count, function (err, data) {
+            ServiceCall.get(URL + data.hash, function (data, response) {
+                var arrayOfObjects = { LastBlock: count };
+                fs.writeFile('./data.json', JSON.stringify(arrayOfObjects), 'utf-8', function (err) {
+                    if (err) throw err
+
+                    console.log('Block No :'+count);
+                    count++;
+                   test(count);
+                })
+                
+            });
+        });
+    }
+    else{
+        LoadFun();
+
+    }
+    
+}
+     catch (e) {
+         console.log('Error : test' + e);
+    }
+}
+LoadFun();
+ 
